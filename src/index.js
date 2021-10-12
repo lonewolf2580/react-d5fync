f549cdebc9af3c7b69ae157703a88e0dd5943843
lonewolf2580 <linusngwee@yahoo.com>
2021-10-13 00:20:01 +0100

    2 files modified, 1 deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
 
-import App from "./App";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
